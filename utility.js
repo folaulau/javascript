862fc34f272211949bc50a3d03c057fef6d1067d
folaulau <folaukaveinga@gmail.com>
2020-10-30 20:53:37 -0600

    added all

--- utility.js
+++ utility.js
@@ -5,6 +5,27 @@ let Utils = {
     copyObject(obj){
         let jsonString = JSON.stringify(obj);
         return JSON.parse(jsonString);
+    },
+
+    utcToLocalDateTime(utcDate){
+        if(utcDate==undefined || utcDate==null){
+            return new Date().toLocaleDateString()+" "+new Date().toLocaleTimeString()
+        }
+        return new Date(utcDate).toLocaleDateString()+" "+new Date(utcDate).toLocaleTimeString();
+    },
+
+    utcToLocalDate(utcDate){
+        if(utcDate==undefined || utcDate==null){
+            return new Date().toLocaleDateString();
+        }
+        return new Date(utcDate).toLocaleDateString();
+    },
+
+    utcToLocalTime(utcDate){
+        if(utcDate==undefined || utcDate==null){
+            return new Date().toLocaleTimeString()
+        }
+        return new Date(utcDate).toLocaleTimeString();
     }
 }
 
